8f93e87dae0acbe026974e84b2da61da722346ae
Nehemiah I. Dacres <vivacarlie@gmail.com>
2014-08-24 02:19:30 -0500

    fixed directory traversal

--- graphStore/tokenizer.py
+++ graphStore/tokenizer.py
@@ -3,12 +3,13 @@
 import operator
 from collections import OrderedDict
 import os
+from os import path
 import json
 import nltk 
 import sys
 import csv
 from nltk.corpus import brown, stopwords
-from nltk.colocations import *
+from nltk.collocations import *
 from neo4jrestclient.client import GraphDatabase
 
 NEO4J_IP_ADDRESS = 'localhost:7474/db/data/'
@@ -17,7 +18,6 @@ def parseNoun( textfile ) :
     sentences = nltk.sent_tokenize(textfile.read().lower())
     words = [nltk.pos_tag(nltk.word_tokenize(sent)) for sent in sentences]
     nounClassSet= set(['NN','NNP','NNS','NP'])
-    
     stopwordsSet = set(stopwords.words('english'))
     #nouns = [[token[0] for token in sents if token[1] in nounClassSet  and token[0] not in stopwordsSet] for sents in words]
     nouns = {}
@@ -33,9 +33,9 @@ def parseNoun( textfile ) :
     
 def makeColocations(document):
     # collocation trigrams
-    trigram_measures - nltk.colocations.BigramAssocMeasures()
+    trigram_measures =  nltk.collocations.BigramAssocMeasures()
     finder = TrigramCollocaionFinder.from_words(document)
-    trigrams = finder.nbest(bigram_measures.pmi, 3)
+    trigrams = finder.nbest(trigram_measures.pmi, 3)
     for tri in trigrams: 
         storeCollocations(tri)
 
@@ -46,17 +46,24 @@ def storeCollocations(trigram):
     gdb.relationships.create(token = trigram[1] )
     gdb.nodes.create(token = trigram[2] )
 
-def ingest(directory):
+def ingest(absolutePath):
+    print os.listdir(absolutePath)
     for document in os.listdir(directory):
-        f = open(document)
-        text = f.read()
+        print os.path.join(absolutePath,document)
+        dirpath = os.path.join(absolutePath,document)
+        if not os.path.isdir(dirpath):
+            f = open(dirpath)
+            text = f.read()
+            makeColocations(text)
+
 
         
 
 
 if __name__ == '__main__':
     directory = sys.argv[1]
-    ingest(directory)
+    print sys.argv[1]
+    ingest(os.path.normpath(directory))
     #mapping = parseNoun(text)
     #writeOut = open(sys.argv[2],'w' )
     #lis  = [(key,mapping[key]) for key in sorted(mapping,key=lambda key: mapping[key], reverse=True)]
